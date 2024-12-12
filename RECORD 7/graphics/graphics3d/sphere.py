ee6c73526554afde689f676a56373d43958ce852
muhammednihal1 <muhammednihall@proton.me>
2024-12-12 15:54:31 +0530

    Add files via upload

---
+++ RECORD 7/graphics/graphics3d/sphere.py
@@ -0,0 +1,5 @@
+def spherearea(r):
+    print("Surface area of sphere: ", 4 * 3.14 * r * r)
+
+def sphereperi(r):
+    print("Circumference of sphere: {:.2f}".format(2 * 3.14 * r))
---
+++ RECORD 7/graphics/rectangle.py
@@ -0,0 +1,5 @@
+def rectarea(l, b):
+    print("Area of Rectangle: ", l * b)
+
+def rectperi(l, b):
+    print("Perimeter of Rectangle: ", 2 * (l + b))
